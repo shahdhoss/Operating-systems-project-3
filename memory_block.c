7d5cfa993fe936f0ea768afad1724475556975c7
shahdhoss <s-shahd.fouda@zewailcity.edu.eg>
2024-05-03 19:24:08 +0300

    added the adjust and find functions.

--- memory_block.c
+++ memory_block.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/mman.h>
+#include <math.h>
 struct head {
 	uint16_t bfree;
 	uint16_t bsize;
@@ -81,6 +82,36 @@ void insert (struct head* block) {
 	}
 	flist=block;
 }
+int adjust(size_t request){
+	int blocks=request % ALIGN;
+	if(blocks==round(blocks)){
+		return blocks;
+	}
+	return ceil(blocks);
+}
+struct  head* find(int size){
+	int blocks=adjust(size);
+	if(flist==NULL){
+		return NULL;
+	}
+	struct head* ptr=flist;
+	struct head* block=flist;
+	block->size=0;
+	while(ptr->next!=NULL){
+		if(blocks!=0){
+			blocks--;
+			if(ptr->size <= size){
+				block->size=block->size+ptr->size;
+				size = size - ptr->size;
+			}
+			else if (ptr->size > size && size!=0){
+				struct head* splt=split(ptr,size);
+				block->size=block->size+splt->size;
+			}
+		}
+	}
+	return block;
+}
 void *dalloc(size_t request){
 	if(request<=0){
 		return NULL;
@@ -101,7 +132,6 @@ int main() {
     detach(block1);
     free(block1);
     free(block2);
-    
     return 0;*/
 }
 
